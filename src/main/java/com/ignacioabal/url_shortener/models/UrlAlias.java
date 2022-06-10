e44d39e28e1c3a7a45485c3b2fd5673342e72331
Ignacio Abal <ignacioabal@users.noreply.github.com>
2022-06-09 22:30:43 -0300

    Added equals and Hash methods to class.

--- src/main/java/com/ignacioabal/url_shortener/models/UrlAlias.java
+++ src/main/java/com/ignacioabal/url_shortener/models/UrlAlias.java
@@ -39,4 +39,23 @@ public class UrlAlias {
     public void setAlias(String alias) {
         this.alias = alias;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UrlAlias urlAlias = (UrlAlias) o;
+
+        if (url != null ? !url.equals(urlAlias.url) : urlAlias.url != null) return false;
+        return alias != null ? alias.equals(urlAlias.alias) : urlAlias.alias == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = url != null ? url.hashCode() : 0;
+        result = 31 * result + (alias != null ? alias.hashCode() : 0);
+        return result;
+    }
+
 }
